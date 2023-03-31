b8f5229c10bfd218c16da622b6624c160765dc65
Diegovitch <86436935+Diegovitch@users.noreply.github.com>
2023-03-31 12:23:55 -0400

    Add files via upload

--- Python.py
+++ Python.py
@@ -1,4 +1,41 @@
-#1 crear una variable:
-Hola_mundo = "Hola"
-#Variable con valor 
-number = 2
+#1. Variable en Python: Para crear una Variable en python tienes que poner solo el nombre de una variable
+# Hola_mundo = "Hola"
+# Variable con valor númerico
+# number = 2
+
+#Como sumar variables con valores númericos
+# number = 3
+# number2 = 2
+# suma = number + number2
+
+#Como Asignar a multiples valores a multiples variables
+# a,b,c = 15, 8, "Fua"
+# print(a)
+# print(b)
+# print(c)
+
+#Asignar un mismo valor a multiples variables
+# k = l = q = 150
+
+#2. La función input: El objetivo de la instrucción input es parar un programa en Python, esperar que el usuario introduzca un dato mediante el teclado
+# nombre = input("Introduzca su Nombre")
+# print("Un gusto", nombre)
+
+# La función int: Esta es raíz del input, sirve para hacer calculos arimeticos
+# valor = int(input("Introduzca un valor que sera divido entre 2"))
+# print(valor, "es igual a ", round(valor/2))
+
+#3. La función Complex: La función complex devuelve un número imaginario a partir de los valores cedidos como argumentos para la parte real e imaginaria del mismo
+# valor = 100
+# complex(valor)
+
+#4. La función Float: El tipo numérico float permite representar un número positivo o negativo con decimales, es decir, números reales
+valor = 50
+float(valor)
+
+#5. La función Int: La función Int convierte un valor decimal a un valor entero
+# decimal = 3.5
+# int(decimal)
+
+#6. strings = son cadenas de texto simplemente
+"Hola mundo"
