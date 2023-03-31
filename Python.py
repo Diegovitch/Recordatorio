aa00345a656624c7daccefca06ba3e33b9718ebb
Diegovitch <86436935+Diegovitch@users.noreply.github.com>
2023-03-31 13:52:35 -0400

    Add files via upload

--- Python.py
+++ Python.py
@@ -1,41 +1,95 @@
 #1. Variable en Python: Para crear una Variable en python tienes que poner solo el nombre de una variable
-# Hola_mundo = "Hola"
-# Variable con valor númerico
-# number = 2
+ # Hola_mundo = "Hola"
+ # Variable con valor númerico
+ # number = 2
 
-#Como sumar variables con valores númericos
-# number = 3
-# number2 = 2
-# suma = number + number2
+ #Como sumar variables con valores númericos.
+ # number = 3
+ # number2 = 2
+ # suma = number + number2
 
-#Como Asignar a multiples valores a multiples variables
-# a,b,c = 15, 8, "Fua"
-# print(a)
-# print(b)
-# print(c)
+ #Como Asignar a multiples valores a multiples variables.
+ # a,b,c = 15, 8, "Fua"
+ # print(a)
+ # print(b)
+ # print(c)
 
-#Asignar un mismo valor a multiples variables
-# k = l = q = 150
+#Asignar un mismo valor a multiples variables.
+ # k = l = q = 150
 
-#2. La función input: El objetivo de la instrucción input es parar un programa en Python, esperar que el usuario introduzca un dato mediante el teclado
-# nombre = input("Introduzca su Nombre")
-# print("Un gusto", nombre)
+#2. La función input: El objetivo de la instrucción input es parar un programa en Python, esperar que el usuario introduzca un dato mediante el teclado.
+ # nombre = input("Introduzca su Nombre")
+ # print("Un gusto", nombre)
 
-# La función int: Esta es raíz del input, sirve para hacer calculos arimeticos
-# valor = int(input("Introduzca un valor que sera divido entre 2"))
-# print(valor, "es igual a ", round(valor/2))
+#3. La función int: Esta es raíz del input, sirve para hacer calculos arimeticos.
+ # valor = int(input("Introduzca un valor que sera divido entre 2"))
+ # print(valor, "es igual a ", round(valor/2))
 
-#3. La función Complex: La función complex devuelve un número imaginario a partir de los valores cedidos como argumentos para la parte real e imaginaria del mismo
-# valor = 100
-# complex(valor)
+#4. La función Complex: La función complex devuelve un número imaginario a partir de los valores cedidos como argumentos para la parte real e imaginaria del mismo.
+ # valor = 100
+ # complex(valor)
 
-#4. La función Float: El tipo numérico float permite representar un número positivo o negativo con decimales, es decir, números reales
-valor = 50
-float(valor)
+#5. La función Float: El tipo numérico float permite representar un número positivo o negativo con decimales, es decir, números reales.
+ #valor = 50
+ #float(valor)
 
-#5. La función Int: La función Int convierte un valor decimal a un valor entero
-# decimal = 3.5
-# int(decimal)
+#6. La función Int: La función Int convierte un valor decimal a un valor entero.
+ # decimal = 3.5
+ # int(decimal)
 
-#6. strings = son cadenas de texto simplemente
-"Hola mundo"
+#7. strings: son cadenas de texto simplemente.
+ #"Hola mundo"
+
+#8. Operadores arimeticos: los Operadores arimeticos son los que se usan para diferenciar entre un valor a otro, aqui uno de ellos.
+ # <: Menor que.
+ # >: Mayor que.
+ # !=: Diferente de.
+ # ==: Igual que.
+ # <=: Menor o igual que.
+ # >=: Mayor o igual que.
+
+#9. Operadores logicos: Los operadores logicos siven como condicionales, uno de ellos son and, not y or.
+ #El Operador and funciona si hay 2 valores, si los 2 valores son iguales la el operador saldrá con un true de lo contrario saldrá con un false.
+ #El operador or funciona si una variable tiene un valor, si no hay una variable con valor salrá con un false si es lo contrario es true.
+ #El operador not sirve si una variable si no tiene un valor, si no tiene un valor es true y si es lo contrario es false.
+ 
+ #Ejemplos:
+ #A = False
+ #B = True
+ #A and B, Dará falso.
+ #A or B, Dará true.
+ #not A, Dará true
+
+#10. Operadores de asignación: Son operadores que basicamente sirven con los operadores arimeticos
+
+ #Ejemplos:
+ 
+ #Con sumas
+ #a = 2 + 2 
+ #a += 2
+
+ #Con resta
+ #a = 2+2
+ #a -=2 
+
+ #Con multiplicación
+ #a = 2 + 2
+ #a *= 2
+
+ #Con división de decimales
+ #a = 3 + 2
+ #a /= 2
+
+ #Con división entera
+ #a = 3 + 2
+ #a //= 2
+
+ #Con porcentaje
+ #a = 2 + 2
+ #a %= 50
+
+#11. Las condicionales: las condicionales son comandos que normalmente se usan para hacer una acción muy precisa y especifica
+ #los comandos:
+ #if: este comando se utiliza si la condicional se cumple, para que haga otra acción
+ #else: este comando se utiliza si no se cumple la condicional, y tomara otras acciones
+ #or: este comando se utiliza cuando la condicional es verdadera hace una acción, si no es verdadera lo devuelva y no hace nada
